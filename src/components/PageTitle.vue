3a913b70b8f4825f3e0651a194da39e0d9214c9b
Joe Bocock <jbocock@lincoln.ac.uk>
2020-05-29 00:07:44 +0100

    Vue-Typer!

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,14 +1,41 @@
 <template>
-    <div class="title">
-        <h2>{{ title }}<span class="blink">|</span></h2>
+    <div class="title spoon">
+        <h2>{{ title }}</h2>
+        <h4><vue-typer
+            :text="typerItems"
+            :erase-delay="eraseDelay"
+            :pre-erase-delay="preEraseDelay"
+            :pre-type-delay="preTypeDelay"
+            caret-animation="phase"
+            erase-style="backspace"
+        ></vue-typer></h4>
     </div>
 </template>
 
 <script>
+import { VueTyper } from 'vue-typer'
+
 export default {
     name: 'PageTitle',
+    components: {
+        VueTyper
+    },
     props: {
         title: String
+    },
+    data: function() {
+        return {
+            typerItems: [
+                'Software Developer ',
+                'Full Stack ',
+                'JS Fanatic ',
+                'Data Centric ',
+                'AI Worshipper '
+            ],
+            eraseDelay: 70,
+            preEraseDelay: 3000,
+            preTypeDelay: 1000
+        }
     }
 }
 </script>
