da40d6750d3af38f5a6c4b9bf99949856a862921
Joe Bocock <jbocock@lincoln.ac.uk>
2020-05-29 23:26:40 +0100

    Vue instance property added to handle if device is mobile globally.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import './assets/css/custom.css'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$isMobile = (() => (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)))();
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
