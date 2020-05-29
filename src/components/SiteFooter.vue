c395a107d248e0a3c5649f1f34896a7a040e8d0b
Joe Bocock <joebocock@gmail.com>
2020-05-29 23:29:37 +0100

    New App.vue child components!

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,25 @@
+<template>
+    <section id="footer">
+        <div class="center" id="version-footer">
+            <small> Version
+                <span class="brand-color">
+                    <strong>{{ version }}</strong>
+                </span> | Powered by
+                <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer" class="no-highlight">
+                    <span class="vue-label">
+                        <strong>Vue</strong>
+                    </span>
+                </a>
+            </small>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SiteFooter',
+    props: {
+        version: String
+    }
+}
+</script>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <section id="header">
+        <PageTitle :title="title" />
+        <hr>
+        <Navbar/>
+        <hr>
+    </section>
+</template>
+
+<script>
+import PageTitle from './PageTitle.vue'
+import Navbar from './Navbar.vue'
+
+export default {
+    name: 'SiteHeader',
+    components: {
+        PageTitle,
+        Navbar
+    },
+    data: function() {
+        return {
+            title: 'Joe Bocock'
+        }
+    },
+    methods: {
+        requestContent(childId) {
+            this.$parent.activateContent(childId);
+        }
+    }
+}
+</script>
