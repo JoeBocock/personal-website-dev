abd171622017a6818795e894f650a76cf9ccca02
Joe Bocock <joebocock@gmail.com>
2020-05-30 09:41:10 +0100

    Command line handler! Still requires display of errors.

--- src/components/DesktopInput.vue
+++ src/components/DesktopInput.vue
@@ -1,12 +1,72 @@
 <template>
-    <input
-        v-if="!$isMobile"
-        type="text"
-    >
+    <form @submit.prevent="handleInput" ref="inputContainer">
+        <input
+            v-if="!$isMobile"
+            v-model="input"
+            ref="commandLine"
+            type="text"
+        >
+    </form>
 </template>
 
 <script>
 export default {
-    name: 'DesktopInput'
+    name: 'DesktopInput',
+    data: function() {
+        return {
+            input: '',
+            inputPrefixes: ['cd', 'nano'],
+            sections: {
+                bio: 1,
+                experience: 2,
+                work: 3,
+                contact: 4
+            }
+        }
+    },
+    methods: {
+        handleInput() {
+            let inputSet = this.prepareInput(this.input);
+            let response = this.validateInput(inputSet);
+
+            if (!response.success) {
+                console.log(response.message);
+            } else {
+                this.$emit('commandLineRequest', this.sections[inputSet[1]]);
+                this.input = '';
+            }
+        },
+        prepareInput(command) {
+            return command.trim().toLowerCase().split(' ').filter(item => item);
+        },
+        validateInput(inputSet) {
+            let response = {
+                success: false,
+                message: ''
+            };
+
+            while (response.message.length === 0) {
+                if (!inputSet || inputSet.length !== 2) {
+                    response.message = 'Two arguments required';
+                    break;
+                }
+
+                if (!this.inputPrefixes.includes(inputSet[0])) {
+                    response.message = 'Invalid command';
+                    break;
+                }
+
+                if (!(inputSet[1] in this.sections)) {
+                    response.message = 'No such file or directory';
+                    break;
+                }
+
+                response.success = true;
+                response.message = 'Valid entry';
+            }
+
+            return response;
+        }
+    }
 }
 </script>
