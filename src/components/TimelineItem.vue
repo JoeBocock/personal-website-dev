efb62c177f3406ff76a74a8274be51c41039adc9
Joe Bocock <joebocock@gmail.com>
2020-05-31 17:14:17 +0100

    New TimelineItem component!

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="timeline-item">
+        <header>{{ title }}</header>
+        <div v-html="content" />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'TimelineItem',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
