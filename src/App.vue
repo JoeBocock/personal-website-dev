dc7c829d685670843ce0cc14c3f391e77b4855fc
Joe Bocock <joebocock@gmail.com>
2020-07-14 18:17:40 +0100

    Layout

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,30 @@
         <div class="bg-image"></div>
 
         <div id="window">
-            <div id="bar"></div>
+            <div id="bar">
+                <div id="window-title">
+                    <img src="./assets/images/mouse.png" alt="" />
+                    <strong>Microdoft Messenger</strong>
+                </div>
+                <div id="window-icons">
+                    <div id="primary-icon" class="bar-icon">
+                        <strong>_</strong>
+                    </div>
+                    <div id="error-icon" class="bar-icon">
+                        <strong>X</strong>
+                    </div>
+                </div>
+            </div>
             <div id="screen"></div>
         </div>
     </div>
 </template>
 
 <script>
-import { lineDraw } from './animation/animate';
-
 export default {
     name: 'App',
     components: {},
-    mounted: function() {
-        lineDraw();
-    },
+    mounted: function() {},
     data: function() {
         return {};
     },
