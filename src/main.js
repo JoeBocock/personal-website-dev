399942442ed6d627ce836498cbf03d8db52f2e10
Joe Bocock <joebocock@gmail.com>
2020-08-12 17:49:40 +0100

    Make a snake game?

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ Vue.config.productionTip = false
 
 Vue.prototype.$isMobile = (() => (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)))();
 
+console.log(`/================================\\
+|                                |
+|     Having a poke around?      |
+|                                |
+|     Don't let me stop you!     |
+|                                |
+|   Try running snake.play();    |
+|                                |
+|               :)               |
+|                                |
+|================================|`);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
