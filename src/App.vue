243cc03e716b30e9145ffbf8f6840cd48303dee5
Joe Bocock <joebocock@gmail.com>
2020-11-01 13:49:16 +0000

    Base App layout.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,51 @@
 <template>
-    <div id="app" class="container">
-        <SiteHeader
-            @showContent="activateContent"
-            :active-content="activeContent"
-        />
-        <SiteContent
-            @commandRequest="activateContent"
-            :active-content="activeContent"
-        />
-        <SiteFooter :version="version" />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">who</router-link>
+      <router-link to="/what">what</router-link>
+      <router-link to="/work">work</router-link>
     </div>
+    <router-view />
+  </div>
 </template>
 
-<script>
-import SiteHeader from './components/SiteHeader.vue';
-import SiteContent from './components/SiteContent.vue';
-import SiteFooter from './components/SiteFooter.vue';
+<style lang="scss">
+@import "./assets/styles/_variables.scss";
 
-export default {
-    name: 'App',
-    components: {
-        SiteHeader,
-        SiteContent,
-        SiteFooter,
-    },
-    data: function() {
-        return {
-            version: '3.1.0',
-            activeContent: 1,
-        };
-    },
-    watch: {
-        activeContent() {
-            window.scrollTo(0, 0);
-        },
-    },
-    methods: {
-        activateContent(contentId) {
-            this.activeContent = contentId;
-        },
-    },
-};
-</script>
+body {
+  background: $background;
+  color: $primary;
+  font-weight: 300;
+  @media screen and (max-width: $small) {
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+  @media screen and (min-width: $medium) {
+    margin-left: 15%;
+    margin-right: 15%;
+  }
+}
+
+#app {
+  font-family: "Montserrat Alternates", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#nav {
+  display: flex;
+  font-size: 1.2rem;
+  justify-content: space-evenly;
+
+  a {
+    text-decoration: none;
+    margin: 30px;
+    color: $primary;
+
+    &.router-link-exact-active {
+      border-bottom: dotted 1px $primary;
+      font-weight: 400;
+    }
+  }
+}
+</style>
